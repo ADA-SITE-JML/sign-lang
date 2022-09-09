7925e7615587d54c17135b491b61e8091c3ed15f
DataMonarch <toghrultahirov2@gmail.com>
2022-09-09 14:54:39 +0400

    Added the necessary DataFrame in CVS format for processing

--- operations/tags_to_sign_language.py
+++ operations/tags_to_sign_language.py
@@ -4,7 +4,7 @@ import os
 
 cwd = os.path.dirname(os.path.abspath("tags_to_sign_language.py"))
 
-parent_dir = cwd + "\\Data\\annotations\\"
+parent_dir = cwd + "\\data\\annotations\\"
 
 # df = pd.read_json(json_path)
 tags_list = []
@@ -21,9 +21,15 @@ for (dirpath, dirnames, filenames) in os.walk(parent_dir):
                 tags = json_dict['tags']
 
                 for i in range(len(tags)):
+                    tags[i]['glossStart'] = tags[i]['frameRange'][0]
+                    tags[i]['glossEnd'] = tags[i]['frameRange'][1]
+                    # print(type(tags[i]['frameRange']))
                     tags[i]['fileName'] = '.'.join(filename.split('.')[:-1])
-                    tags[i]['sentenceIndex'] = sentence_idx
+                    tags[i]['sentenceID'] = sentence_idx
                 tags_list += tags
+data = pd.DataFrame(tags_list).drop(['frameRange', 'labelerLogin', 'updatedAt', 'createdAt', 'key'], axis=1)
+cols = list(data.columns)
 
-data = pd.DataFrame(tags_list).drop(['labelerLogin', 'updatedAt', 'createdAt', 'key'], axis=1)
-data.to_csv(cwd + '\\Data\\processed_gloss.csv')
+cols[0] = 'gloss'
+data.columns = cols
+data.to_csv(cwd + '\\Data\\processed_gloss.csv', index=False)
